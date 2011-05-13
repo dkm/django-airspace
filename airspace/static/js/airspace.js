78d3bcb373aaf645c3635dfd2e5190c1ce2c1ed1
Marc Poulhies <Marc Poulhies marc.poulhies@imag.fr>
2011-05-13 14:24:03 +0200

    static content (js, css, images)

---
+++ airspace/static/js/airspace.js
@@ -0,0 +1,114 @@
+ // -*- coding: utf-8 -*-
+ //   airspace checker
+ //   Copyright (C) 2011  Marc Poulhiès
+
+ //   This program is free software: you can redistribute it and/or modify
+ //   it under the terms of the GNU General Public License as published by
+ //   the Free Software Foundation, either version 3 of the License, or
+ //   (at your option) any later version.
+
+ //   This program is distributed in the hope that it will be useful,
+ //   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ //   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ //   GNU General Public License for more details.
+
+ //   You should have received a copy of the GNU General Public License
+ //   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+var geojsonloader = new OpenLayers.Format.GeoJSON();
+var inter_vectors = new OpenLayers.Layer.Vector("INTERSECTION");
+
+
+function displayIntersection(map, intersection) {
+  var features = geojsonloader.read(intersection);
+  inter_vectors.addFeatures(features);
+  map.addLayers([inter_vectors]);
+}
+
+function displaySingleAirspace(map, aspace_gjson){
+    var geojsonloader = new OpenLayers.Format.GeoJSON({
+	'internalProjection': new OpenLayers.Projection("EPSG:900913"),
+	'externalProjection': new OpenLayers.Projection("EPSG:4326")
+    });
+
+    var features = geojsonloader.read(aspace_gjson);
+    
+    // projection here is useless as it is applied only on objects created by the library.
+    // here, we are creating the objects and then using the addFeature().
+    var vectors = new OpenLayers.Layer.Vector("space", {
+	projection: new OpenLayers.Projection("EPSG:4326")
+    });
+
+    vectors.addFeatures(features);
+    map.addLayers([vectors]);
+}
+
+function displayAirspace(map, aspaces){
+            var report = function(e) {
+	        if(e['feature']['attributes'].hasOwnProperty('name')){
+	             $('#highlighted').html(e['feature']['attributes']['name']);
+	        }
+                OpenLayers.Console.log(e.type, e.feature.id);
+            };
+	    
+            var selected = function(f) {
+	       var str ="Name: " + f['attributes']['name'] + "<br/>";
+	       str += "<ul>";
+	       str += "<li>Floor:";
+	      for (var i=0; i<f['attributes']['floor'].length; i++){
+		  if (f['attributes']['floor'][i]['flevel']){
+                       str+= "[ FL" + f['attributes']['floor'][i]['flevel'] + "]";
+		  } else {
+                      str+= "[" + f['attributes']['floor'][i]['ref'] +" " + f['attributes']['floor'][i]['basealti'] + "]";
+                  }
+              }
+
+	      str += "</li>";
+	       str += "<li>Ceil:";
+	      for (var i=0; i<f['attributes']['ceiling'].length; i++){
+                  if (f['attributes']['ceiling'][i]['flevel']){
+		        str+= "[ FL" + f['attributes']['ceiling'][i]['flevel'] + "]";
+	          } else {
+                  	str+= "[" + f['attributes']['ceiling'][i]['ref'] + " " +f['attributes']['ceiling'][i]['basealti'] + "]";
+		  }
+              }
+
+	      str += "</li>";
+	       str += "<li>Class:" + f['attributes']['class'] + "</li>";
+	       str += "</ul>";
+	       $('#info').html(str);
+            };
+
+        var vectors_layers = new Array();								 
+	for (var i=0; i<aspaces.length; i++) {
+           var features = geojsonloader.read(aspaces[i]);
+           var vectors = new OpenLayers.Layer.Vector(aspaces[i]['features'][0]['properties']['class']);
+	   vectors.addFeatures(features);
+           vectors_layers[i] = vectors;
+        }
+           map.addLayers(vectors_layers);
+
+           var highlightCtrl = new OpenLayers.Control.SelectFeature(vectors_layers, {
+                hover: true,
+                highlightOnly: true,
+                renderIntent: "temporary",
+                eventListeners: {
+                    beforefeaturehighlighted: report,
+                    featurehighlighted: report,
+                    featureunhighlighted: report
+                }
+           });
+
+           var selectCtrl = new OpenLayers.Control.SelectFeature(vectors_layers,
+                {clickout: true,
+                 onSelect: selected
+                }
+           );
+ 
+           map.addControl(highlightCtrl);
+           map.addControl(selectCtrl);
+
+           highlightCtrl.activate();
+           selectCtrl.activate();
+
+}
