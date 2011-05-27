7ea167ee4937c30c85a3da88b25c54addc23a440
Marc Poulhies <Marc Poulhies marc.poulhies@imag.fr>
2011-05-27 19:02:53 +0200

    the bbox query now returns a FeatureCollection GeoJSON object.

--- airspace/views.py
+++ airspace/views.py
@@ -4,20 +4,22 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from django.contrib.gis.geos import Polygon
+import geojson
 
 from django.http import Http404
 
 def json_zone(request, zone_id):
     try:
         z = AirSpaces.objects.get(pk=zone_id)
-        s = z.geom.json
+        s = geojson.dumps(z)
     except AirSpaces.DoesNotExist:
         raise Http404
     return HttpResponse(s, mimetype='application/json')
 
 def json_zone_bbox(request, lowlat, lowlon, highlat, highlon):
     zone_bbox = Polygon(((float(lowlat),float(lowlon)), (float(lowlat), float(highlon)), (float(highlat), float(highlon)), (float(highlat), float(lowlon)), (float(lowlat), float(lowlon))))
-    z = AirSpaces.objects.filter(geom__bboverlaps=zone_bbox)
+    spaces = AirSpaces.objects.filter(geom__bboverlaps=zone_bbox)
+    js_spaces = geojson.FeatureCollection(list(spaces))
 
     return HttpResponse(len(z), mimetype='application/json')
 
